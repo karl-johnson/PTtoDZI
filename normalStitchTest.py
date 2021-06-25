75ee332737da3a0c0e89267b7ef16d157bb4699d
ka-rljohnson <k@rljohnson.net>
2021-06-25 02:07:29 -0600

    Nearly replicated normal PTGui stitching.

--- normalStitchTest.py
+++ normalStitchTest.py
@@ -3,7 +3,7 @@ import os
 import math
 pts_path = 'test/20210612_147megapixels/20210612_pano2.pts'
 output_path, pts_filename = os.path.split(pts_path)
-print(output_path)
+#print(output_path)
 with open(pts_path) as f:
     pts = json.load(f)
 data = pts['project']
@@ -14,9 +14,8 @@ if os.path.exists(nona_script_path):
 nona_script = open(nona_script_path, 'x')
 # Get panorama parameters and write "p" line to script
 nona_script.write('p ')
-# Prior to setting image params, copy lens database
-# todo: E parameters
-# for some reason PTGUI does not store resolution in the files
+# todo: E parameter
+# for some reason PTGUI does not store x and y resolution in the files
 # it only (sometimes) stores the # of pixels in image + X&Y panorama extent
 if 'pixels' in data['outputsize']:
     outputsize = data['outputsize']['pixels']
@@ -29,13 +28,35 @@ width_px = round(math.sqrt((width_deg/height_deg)*outputsize))
 height_px = round(math.sqrt((height_deg/width_deg)*outputsize))
 nona_script.write('w' + str(width_px) + ' ')
 nona_script.write('h' + str(height_px) + ' ')
-nona_script.write('f0 ') # TODO MAKE THIS AUTOMATICALLY SWITCH
-
+nona_script.write('f1 ') # TODO MAKE THIS AUTOMATICALLY SWITCH
 nona_script.write('v' + str(data['panoramaparams']['hfov']) + ' ')
+nona_script.write('u50 ')
 nona_script.write('nJPEG \n')
+# Shortcut to lens database
+globallenses = data['globallenses']
 # Iterate through imagegroups and write "i" lines
 imagegroups = data['imagegroups']
+for image in imagegroups:
+    nona_script.write('i f0 ') # TODO fix projection
+# params to add: d e g t Eev Er Eb Vm Va-d
+    lens = globallenses[image['globallens']]
+    #print(lens)
 
+    diagfov = math.degrees(2*math.atan(lens['lens']['params']['sensordiagonal']/(2*lens['lens']['params']['focallength'])))
+    aspect_ratio = image['size'][1]/image['size'][0]
+    hfov = diagfov*math.sqrt(1/(1+aspect_ratio**2))
+    #hfov = 20
+    nona_script.write('v'+str(hfov)+' ')
+    nona_script.write('y'+str(image['position']['params']['yaw'])+' ')
+    nona_script.write('p'+str(image['position']['params']['pitch'])+' ')
+    nona_script.write('r'+str(image['position']['params']['roll'])+' ')
+    nona_script.write('a'+str(lens['lens']['params']['a'])+' ')
+    nona_script.write('b'+str(lens['lens']['params']['b'])+' ')
+    nona_script.write('c'+str(lens['lens']['params']['c'])+' ')
+
+    nona_script.write('n\"'+image['images'][0]['filename']+'\" \n')
 # reference nona command
+# nona -v -d -g -o nona_stitch_out nona_temp_script.txt
+nona_script.write('*')
 # nona -o nona_stitch_out nona_stitch.txt
 # TODO DELETE TEMP SCRIPT
