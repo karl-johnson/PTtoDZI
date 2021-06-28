7a39fa21299cce77c06fa83e9c7d7981ba19f396
ka-rljohnson <k@rljohnson.net>
2021-06-28 01:59:29 -0600

    Began work on efficient DZI algorithm

--- PTtoDZI.py
+++ PTtoDZI.py
@@ -1,4 +1,7 @@
-import os, PTGUItools, DZItools
+import os, PTGUItools, DZItools, misctools
+STITCH_TILE_SIZE = 32768 # easy if power of 2
+DZI_TILE_SIZE = 256
+DZI_TILE_OVERLAP = 1
 pts_in = os.path.join('test','20210612_147megapixels','20210612_pano2.pts')
 image_out = os.path.join('test','20210612_147megapixels','dzi_test','stitch.jpg')
 dzi_out = os.path.join('test','20210612_147megapixels','dzi_test','stitch_dzi.zip')
@@ -10,8 +13,16 @@ if pts['project']['panoramaparams']['projection'] != 'cylindrical':
     print("Error: non-cylindrical projection")
     exit()
 
-# stitch panorama using PTGUI
-PTGUItools.stitch(pts, pts_in, image_out)
+# stitch panorama segments using PTGUI
+res_info = getResolutionInfo(pts)
+stitch_regions = misctools.getTilingRegions(res_info.h, res_info.v, STITCH_TILE_SIZE)
+for region in stitch_regions:
+    # UPDATE DZI FILENAME AND ADD DZIs TO ARRAY
+    PTGUItools.stitch(pts, pts_in, image_out, region)
+    # convert output to DZI using VIPS
+    DZItools.convert(image_out, dzi_out, 256, 0)
+    # delete image_out
 
-# convert output to DZI using VIPS
-DZItools.convert(image_out, dzi_out, 256, 0)
+# MERGE DZI
+# REMEMBER THAT LOWER RIGHT CORNER HAS TO BE TREATED SPECIAL
+DZItools.merge(dzi_list, final_output_location)
---
+++ misctools.py
@@ -0,0 +1,15 @@
+import math
+def getTilingRegions(x_size, y_size, tile_size):
+    # first get no. of tiles in x and y
+    num_x = math.ceil(x_size/tile_size)
+    num_y = math.ceil(y_size/tile_size)
+    output = [[]];
+    for row in range(num_y):
+        for col in range(num_x):
+            x_start = col*tile_size
+            y_start = row*tile_size
+            x_end = min((col+1)*tile_size, x_size) # limit to region
+            y_end = min((row+1)*tile_size, y_size)
+            output.append([x_start, y_start, x_end-x_start, y_end-y_start])
+    output.pop(0)
+    return output
